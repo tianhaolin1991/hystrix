b1a8a9f92bd856745ff85a35ffa638244cffb9ef
tianhaolin1991 <tianhaolin1991@126.com>
2020-01-14 00:14:20 +0800

    Hystrix初级使用

--- parent/user/src/main/java/com/tianhaolin/hystrix/user/controller/RegistrationController.java
+++ parent/user/src/main/java/com/tianhaolin/hystrix/user/controller/RegistrationController.java
@@ -1,11 +1,15 @@
 package com.tianhaolin.hystrix.user.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.tianhaolin.hystrix.user.entity.User;
 import com.tianhaolin.hystrix.user.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 public class RegistrationController {
@@ -14,9 +18,21 @@ public class RegistrationController {
     private ActivityService activityService;
 
     @PostMapping("/userRegistration")
-    public String userRegistration(@RequestBody User user){
+    @HystrixCommand(
+            fallbackMethod = "firstLoginFallBack",
+            commandProperties = {
+                    //HystrixCommandProperties中提供了相关配置
+                    @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000")//设定超时时间
+            }
+    )
+    public String userRegistration(@RequestBody User user) throws InterruptedException {
         System.out.println("用户注册成功:" + user.getName());
+        Thread.sleep(new Random().nextInt(4000));
         return activityService.firstLogin(user.getId());
+    }
+
+    public String firstLoginFallBack(@RequestBody User user){
+        return "fallback";
     }
 
 }
